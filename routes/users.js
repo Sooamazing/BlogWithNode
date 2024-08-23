8fcd49f105f462204ed080cb024437156b311be7
Sooamazing <honang515@gmail.com>
2024-08-24 01:17:27 +0900

    feat: GET users/:id

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,16 @@ router.get("/", (req, res) => {
 }); // sets up a route that responds to HTTP GET requests.
 // when a user hits the GET URL, the server responds by sending the data inside the users variable in JSON format to the client.
 
+// GET :id
+// if not exist, just nothing.
+router.get("/:id", (req, res) => {
+  const { id } = req.params; // query string
+
+  const foundUser = users.find((user) => user.id === id);
+
+  res.send(foundUser);
+});
+
 // POST
 // sets up a route that responds to HTTP POST requests
 router.post("/", (req, res) => {
