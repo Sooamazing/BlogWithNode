5dcb43f5b8b82f1c45ad2b7ea36d73d109e99678
Sooamazing <honang515@gmail.com>
2024-08-24 00:52:38 +0900

    feat: POST /users

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,25 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router(); // creates a fresh router instance, stored in the variable router.
 
-// Mock database
-const users = [
-  {
-    first_name: "John",
-    last_name: "Doe",
-    email: "johndoe@example.com",
-  },
-  {
-    first_name: "Alice",
-    last_name: "Smith",
-    email: "alicesmith@example.com",
-  },
-];
+const users = [];
 
-// Getting the list of users from the mock database
+// GET
 router.get("/", (req, res) => {
   // It gets executed when a request is made to the GET route.
   res.send(users); // to send a response back to the client
 }); // sets up a route that responds to HTTP GET requests.
 // when a user hits the GET URL, the server responds by sending the data inside the users variable in JSON format to the client.
+
+// POST
+// sets up a route that responds to HTTP POST requests
+router.post("/", (req, res) => {
+  const user = req.body;
+
+  users.push({ ...user, id: uuidv4() });
+
+  res.send(`${user.first_name} has been added to the Database`);
+});
 
 export default router;
